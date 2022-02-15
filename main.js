3e2383bf786d95429723baaa369649d91adc14c5
a-wing <1@233.email>
2022-02-15 17:13:16 +0800

    feat(main): first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import browserslist from 'browserslist'
+import fs from 'fs'
+
+const list = {
+  'firefox': true,
+  'chrome': true,
+  'edge': true,
+}
+
+const latest = browserslist("last 1 version").filter(i => list[i.split(' ')[0]]).reduce((r, i) => {
+	r[i.split(' ')[0]] = i.split(' ')[1]
+	return r
+}, {})
+
+//fs.writeFileSync('./browser_compatibility_latest_browser_list.js', `export default ${JSON.stringify(latest)}`)
+
+
+const dir = './build';
+if (!fs.existsSync(dir)){
+	fs.mkdirSync(dir);
+}
+
+fs.writeFileSync(`${dir}/list.json`, JSON.stringify({ latest: latest }))
+
+console.log({ latest: latest })
+//console.log(`Update: ${latest}`)
